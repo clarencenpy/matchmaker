d2662867122e57dfc64e1f623e4ba2f0b715ffcc
Clarence Ngoh <py.ngoh.2014@smu.edu.sg>
2015-12-08 19:53:40 +0900

    add check compatibility modal

--- public/app.js
+++ public/app.js
@@ -20,6 +20,7 @@ var Data = {
             image: "http://sis.smu.edu.sg/sites/default/files/sis/students/BSc-ISM/2013/lee_chian_yee.jpg",
             interests: ['badminton','baking','cooking'],
             views: 12, //num of people that flipped the card
+            likes: 0,
             flowers: [
                 {
                     flower: "Rose",
@@ -35,6 +36,7 @@ var Data = {
             sex: "male",
             status: "red",
             views: 2,
+            likes: 0,
             displayMessage: "I'm sexy and i know it!",
             image: "http://sis.smu.edu.sg/sites/default/files/sis/students/BSc-ISM/2013/lee_chian_yee.jpg",
             interests: ['basketball','sports']
@@ -46,6 +48,7 @@ var Data = {
             sex: "male",
             status: "orange",
             views: 0,
+            likes: 0,
             displayMessage: "I eat well",
             image: "http://sis.smu.edu.sg/sites/default/files/sis/students/BSc-ISM/2013/lee_chian_yee.jpg",
             interests: ['eating']
@@ -65,6 +68,7 @@ var App = {
         //check compatibility button
         $peopleList.on('click', '.btn-check', function () {
             var id = $(this).parents('.ui.card').data("id");
+            App.checkCompatibility(id);
         });
 
         //send gift button
@@ -93,6 +97,35 @@ var App = {
     },
     checkCompatibility: function (id) {
 
+        //inject template then show the modal
+        var template = $('#compare-template').html();
+        
+        //retrieve the user to compare to
+        var other = _.find(Data.people, function (p) {
+           return p.id === id;
+        });
+
+        var data = {};
+        //calculate common interest percent
+        //intersecting interests / total interests
+        //OR intersecting interests / own interests
+
+        data.interestPercent = _.intersection(Data.self.interests, other.interests) / _.union(Data.self.interests, other.interests) * 100;
+        data.astrologyPercent = 66;
+        data.self = Data.self;
+        data.other = other;
+
+        //inject template
+        $('#compare-modal').html(Mustache.render(template, data));
+
+        //show modal
+        $('#compare-modal')
+            .modal('show')
+        ;
+
+        //init progress bars
+        $('#interest-progress').progress();
+        $('#astrology-progress').progress();
     }
 };
 
