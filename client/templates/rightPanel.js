60be82dd4caf958d597abd86736f3a9625d35c01
Clarence Ngoh <py.ngoh.2014@smu.edu.sg>
2015-12-14 05:48:31 -0500

    add check compatibility feature

--- client/templates/rightPanel.js
+++ client/templates/rightPanel.js
@@ -6,7 +6,31 @@ Template.rightPanel.onRendered(function () {
 });
 
 Template.rightPanel.helpers({
-   people: function () {
-       return Data.people;
-   }
+    people: function () {
+        return Data.people;
+    },
+    compare: function () {
+        return Session.get('activeComparison');
+    }
+});
+
+Template.rightPanel.events({
+    'click .btn-check': function () {
+        var other = this;
+        var activeComparison = {};
+        activeComparison.interestPercent = _.intersection(Data.self.interests, other.interests).length / _.union(Data.self.interests, other.interests).length * 100;
+        activeComparison.other = other;
+
+        Session.set('activeComparison',  activeComparison);
+
+        //show modal
+        $('#compare-modal')
+            .modal('show')
+        ;
+
+        //init progress bars
+        $('#interest-progress').progress({percent: activeComparison.interestPercent});
+        $('#astrology-progress').progress();
+
+    }
 });
